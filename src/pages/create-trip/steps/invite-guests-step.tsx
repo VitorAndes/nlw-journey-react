9ce6b579851f0a58cf148cdd904163b49ae3d754
Vitor Andes <vitor.andes.santos04@gmail.com>
2024-07-10 16:33:19 -0300

    aula02 - add pages/react router dom, components, Activities, Important Links, Guests

---
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -0,0 +1,41 @@
+import { UserRoundPlus, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
+
+interface InviteGuestsStepProps {
+  openGuestsModal: () => void;
+  emailsToInvite: string[];
+  openConfirmTripModal: () => void;
+}
+
+export function InviteGuestsStep({
+  emailsToInvite,
+  openConfirmTripModal,
+  openGuestsModal,
+}: InviteGuestsStepProps) {
+  return (
+    <div className="h-16  bg-zinc-900 p-4 rounded-xl flex items-center shadow-shape gap-3">
+      <button
+        type="button"
+        onClick={openGuestsModal}
+        className="flex items-center gap-2 flex-1"
+      >
+        <UserRoundPlus className="size-5 text-zinc-400" />
+        {emailsToInvite.length > 0 ? (
+          <span className="text-lg text-zinc-100 flex-1 text-left">
+            {emailsToInvite.length} pessoa(s) convidada(s)
+          </span>
+        ) : (
+          <span className="text-lg text-zinc-400 flex-1 text-left">
+            Quem estará na viagem?
+          </span>
+        )}
+      </button>
+
+      <div className="w-px h-6 bg-zinc-800" />
+      <Button onClick={openConfirmTripModal} variant="primary">
+        Confimar viagem
+        <ArrowRight className="soze-5 text-lime-950" />
+      </Button>
+    </div>
+  );
+}
---
+++ src/pages/trip-details/guests.tsx
@@ -0,0 +1,36 @@
+import { CircleDashed, UserCog } from "lucide-react";
+import { Button } from "../../components/button";
+
+export function Guests() {
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Convidados</h2>
+      <div className="space-y-5">
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">
+              Vitinhu gamer 12
+            </span>
+            <span className="block text-sm text-zinc-400 truncate">
+              VitinhuGamer12@gmail.com
+            </span>
+          </div>
+          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+        </div>
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">Dreyzin</span>
+            <span className="block text-sm text-zinc-400 truncate">
+              Dreyzin@gmail.com
+            </span>
+          </div>
+          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+        </div>
+      </div>
+
+      <Button variant="secondary" size="full">
+        <UserCog className="size-5" /> Gerenciar convidados
+      </Button>
+    </div>
+  );
+}
